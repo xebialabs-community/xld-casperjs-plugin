625e4e5002820020bc5109231d89cd6abd64107e
Benoit Moussaud <bmoussaud@xebialabs.com>
2014-12-09 21:09:48 +0100

    licence format

--- src/main/resources/phantomjs/runtime/casperjs/tests/clitests/tester/casper-instance-override.js
+++ src/main/resources/phantomjs/runtime/casperjs/tests/clitests/tester/casper-instance-override.js
@@ -1,3 +1,8 @@
+/*
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+ * FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+ */
 // this should never happen
 // http://docs.casperjs.org/en/latest/testing.html#test-command-args-and-options
 var casper = require("casper").create();
